9217f0cea0eee300e1fe13708cb91c0ca4f3bec8
Alexis Merino <alexismerino49@gmail.com>
2024-06-11 14:31:21 -0500

    added tailwind and email.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import "./App.css"
-import"../src/components"
+import {About, Contact, Hero, Experience, Projects,Navbar} from "./components"
 
 const App = () => {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+// exports all the components 
+
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+
+
+
+export {
+    Hero,
+    Navbar,
+    About,
+    Experience,
+    Projects,
+    Contact,
+};
