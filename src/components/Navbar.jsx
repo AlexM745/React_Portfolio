f5ccf293e74f7f10bc16b70626f739165a6e1265
Alexis Merino <alexismerino49@gmail.com>
2024-06-11 15:07:14 -0500

    removed app.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import logo from "../assets/logo.svg"
+import { Link } from "react-router-dom"
 
-
+// this is the nav bar information.
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div>NAV Bar</div>
+        <nav className="w-full flex items-center py-5 fixed top-0 z-20">
             <img src={logo}alt="logo" className="logo" />
             <div className="menu"></div>
-            
+            <link></link>
             <button className="menuBtn">
                 <img src="" alt="" /> Contact Me
             </button> 
