09e2350fb90866664593c4ae4a9eca9ae8a127a7
Alexis Merino <alexismerino49@gmail.com>
2024-06-11 19:09:35 -0500

    added name and logo to link in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import React from "react"
-import logo from "../assets/logo.svg"
-import { Link } from "react-router-dom"
 
-// this is the nav bar information.
+import React, {useEffect, useState} from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
+// this is the nav bar section of the page.
 
 const Navbar = () => {
-    return (
-        <nav className="">
-            <img src={logo}alt="logo" className="logo" />
-            <div className="menu"></div>
-            <Link className="menuListItem">Home</Link>
-            <Link className="menuListItem">About</Link>
-            <Link className="menuListItem">Portfolio</Link>
-            <Link className="menuListItem">Experience</Link>
-            <button className="menuBtn">
-                <img src="" alt="" /> Contact Me
-            </button> 
+    const [active, setActive] = useState('');
 
+    return (
+        // nav container
+        <nav className="sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
+
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+                <Link to="/" className="flex items-center gap-2"
+                onClick={()=>{
+                    setActive("");
+                    window.scroll(0,0);
+                }} >
+                    <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+                    <p className="text-white text-[18] font bold cursor-pointer">Alexis Merino</p>
+                    
+                </Link>
+            </div>
         </nav>
     )
 }
