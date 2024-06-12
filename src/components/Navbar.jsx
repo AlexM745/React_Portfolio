7bedba3c9ba642752daafc574134f8b901ad63cc
Alexis Merino <alexismerino49@gmail.com>
2024-06-12 10:12:11 -0500

    added menu icon and links on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.svg";
+import menu from "../assets/menu.png";
+import close from "../assets/close.png";
 import { Link } from "react-router-dom";
 
 // this is the nav bar section of the page.
 
 const Navbar = () => {
     const [active, setActive] = useState('');
+    const [toggle, setToggle] = useState(false);
 
     return (
         // nav container
@@ -14,14 +17,38 @@ const Navbar = () => {
 
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <Link to="/" className="flex items-center gap-2"
-                onClick={()=>{
-                    setActive("");
-                    window.scroll(0,0);
-                }} >
-                    <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-                    <p className="text-white text-[18] font bold cursor-pointer">Alexis Merino</p>
-                    
+                    onClick={() => {
+                        setActive("");
+                        window.scroll(0, 0);
+                    }} >
+                    <img src={logo} alt="logo" className=" w-9 h-9 object-contain" />
+                    <p className="text-white text-[20px] font bold cursor-pointer">Alexis Merino</p>
+
+                {/* The Navigaton links */}
+
                 </Link>
+                <ul className="list-none hidden sm:flex flex-row gap-9">
+                    <li className="text-secondary text-[21px] font-medium cursor-pointer">
+                        {[
+                            ["About", "#about"],
+                            ["Portfolio", "#portfolio"],
+                            ["Experience", "#experience"],
+                            ["Contact", "#contact"],
+                        ].map(([title, url]) => (
+                            <a href={url} className="hover:text-white  p-4">{title}</a>
+                        ))}
+                    </li>
+                </ul>
+
+                {/* Menue icon */}
+
+                <div>
+                    <img src={toggle ? close :menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"
+                        onClick={() => {
+                            setToggle(!toggle);
+                        }} />
+                </div>
+
             </div>
         </nav>
     )
