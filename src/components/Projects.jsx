795e74b0273700cc650324c92d9b22dafae53840
Alexis Merino <alexismerino49@gmail.com>
2024-06-13 15:51:31 -0500

    addded content to the projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,23 @@ import React from "react"
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <>
+            <p className="sm:text-[20px] text-[16px] text-secondary uppercase tracking-wider">
+                My Projects </p>
+            <h2 className="text-white font-black md:text-[60px] sm:text-
+            [50px] xs:text-[40px] text-[30px]">Portfolio.</h2>
+
+
+        <div className="w-full flex">
+            <p className=" mt-3 text-secondary text-[18px] max-w-3l leading-[30px]">
+            Following projects are the reflection of my skills and experience through my Full Stack Bootcamp course. Each project has a brief description with links to their repositories and live demos in it. They show my ability to work with different technologies and manage projects effectively.
+
+            </p>
+        </div>
+
+        <div>project cards go here</div>
+
+        </>
     )
 }
 
