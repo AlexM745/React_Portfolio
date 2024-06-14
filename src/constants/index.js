e93e634d009eb9b8ca18b8991fbd2847a4dcefd3
Alexis Merino <alexismerino49@gmail.com>
2024-06-13 23:29:09 -0500

    added constants and Section wrapper

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+// this file will have all the information so that it is passed to files
+// if i need to edit I can do that in one file
+
+
+const skills =[
+
+]
+
+const projects = [
+
+]
+
+
+const experiences = [
+
+]
+
+// exporting the array of objects so that I can use them on the different sections on jsx files
+
+export { skills, experiences, projects };
