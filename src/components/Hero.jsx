6732b864694753ae3cc6e221e2f5339c2af6bf39
Alexis Merino <alexismerino49@gmail.com>
2024-06-14 12:04:37 -0500

    gots spline log to work

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import Spline from '@splinetool/react-spline';
 import InteractiveLogo from "./InteractiveLogo";
+import {motion} from "framer-motion"
+
 const Hero = () => {
     return (
         // hero section 
         <section className="relative w-full h-screen mx-auto">
-            
-{/* This div contains my name and ocupation. */}
+
+            {/* This div contains my name and ocupation. */}
             <div
-                className="absolute inset-0 top-[120px]  max-w-7xl mx-auto flex sm:px-16 px-6 flex-row items-start gap-5"
+                className="absolute inset-0 top-[120px]  max-w-7xl mx-auto flex sm:px-16 px-6 flex-row items-start gap-5 pointer-events-none"
             >
                 {/* Small circle and fading line */}
                 <div className='flex flex-col justify-center items-center mt-5'>
@@ -28,9 +30,25 @@ const Hero = () => {
                 </div>
             </div>
 
+            <InteractiveLogo/>
 
-            <InteractiveLogo />
-            
+            <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+                <a href='#about'>
+                    <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                        <motion.div
+                            animate={{
+                                y: [0, 24, 0],
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className='w-3 h-3 rounded-full bg-secondary mb-1'
+                        />
+                    </div>
+                </a>
+            </div>
         </section>
     )
 }
--- src/components/InteractiveLogo.jsx
+++ src/components/InteractiveLogo.jsx
@@ -1,7 +1,37 @@
 import Spline from '@splinetool/react-spline';
 
-export default function App() {
+export default function SplineLogo() {
+
     return (
+        <div className=''>
         <Spline scene="https://prod.spline.design/EvtshjtSSAP9FjJw/scene.splinecode" />
+        </div>
+
     );
+}
+
+const Logo = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Add a listener for changes to the screen size
+        const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+        // Set the initial value of the `isMobile` state variable
+        setIsMobile(mediaQuery.matches);
+
+        // Define a callback function to handle changes to the media query
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        // Add the callback function as a listener for changes to the media query
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        // Remove the listener when the component is unmounted
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
+
 }
