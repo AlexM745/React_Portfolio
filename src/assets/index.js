76daba8a0724e2f9a0c7c1d99f6275690e4133e3
Alexis Merino <alexismerino49@gmail.com>
2024-06-13 23:49:46 -0500

    added skills icons and assets index.js

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import backend from "./backend.png";
+import web from "./web.png";
+import react from "./react.png";
+
+export {backend, react, web};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,24 @@
 // this file will have all the information so that it is passed to files
 // if i need to edit I can do that in one file
+ import{ backend, creator, web
 
+ } from '../assets';
 
-const skills =[
+const skills = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "React Developer",
+        icon: creator,
+    },
+];
 
-]
 
 const projects = [
 
