1bffd7ed1d688a48fceee834723b8b78c8245202
Alexis Merino <alexismerino49@gmail.com>
2024-06-18 18:18:02 -0500

    added motion and a sectionwrapper component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Tilt from "react-parallax-tilt"
 import { motion } from "framer-motion";
 import { skills } from "../constants";
+import SectionWrapper from "./SectionWrapper";
 
 // this is the skill cards that will be rendered in the div seciton in about me component.
 const SkillsCard = ({ index, title, icon }) => {
@@ -12,7 +13,7 @@ const SkillsCard = ({ index, title, icon }) => {
             {/* using framer motion to give the cards an animation */}
             <motion.div variants={("right", "spring", 0.5 * index, 0.75)}
                 className="w-full blue-gradient p-[1px] rounded-[20px] shadow-card">
-                    {/* This is the cards for each skill */}
+                {/* This is the cards for each skill */}
                 <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h=[280px] flex justify-evenly items-center flex-col" >
                     {/* Thjs is the icon of each skill */}
                     <img src={icon} alt="skill icons" className="w-16 h-16 object-contain align" />
@@ -30,7 +31,9 @@ const About = () => {
     return (
         <>
             {/* using framer motion to add animation to the text */}
-            <motion.div variants={{}}
+            <motion.div
+                animate={{ x: 100 }}
+                transition={{ type:"spring", ease: "easeOut", duration: 1.25, delay:1}}
             >
                 {/* the header section of the about me  */}
                 <p className="sm:text-[20px] text-[16px] text-secondary uppercase tracking-wider">
@@ -39,7 +42,9 @@ const About = () => {
                     Overview.</h2>
             </motion.div >
             {/* description of myself */}
-            <motion.p variants={("", "", 0.1, 1)}
+            <motion.p  
+            animate={{ x: 100, opacity:1,}}
+                transition={{ type: "spring", ease: "easeOut", duration: 1.25, delay: 1.25}}
                 className=" mt-4 text-secondary text-[20] max-w-3xl leading-[30px] ">
 
                 I am skilled software developer with experience in React, Javascript, HTML, CSS Frameworks, Databases and API.
@@ -58,5 +63,5 @@ const About = () => {
         </>
     )
 }
-
-export default About
+// using a high order component to wrap around the component then the id of the component
+export default SectionWrapper(About, "about")
